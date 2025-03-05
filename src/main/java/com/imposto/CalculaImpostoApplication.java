51bcc35d36a94ad25fbebad58d3db579be8c602d
matheus <matheus.pereira@zup.com.br>
2025-03-05 15:01:09 -0300

    chore: initialize spring project

---
+++ src/main/java/com/imposto/CalculaImpostoApplication.java
@@ -0,0 +1,13 @@
+package com.imposto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculaImpostoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculaImpostoApplication.class, args);
+	}
+
+}

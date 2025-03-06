5edb0821b8232fc57fe8c095e031a18245c5b280
matheus <matheus.pereira@zup.com.br>
2025-03-06 10:25:57 -0300

    feat: add entity UserModel

---
+++ src/main/java/com/imposto/model/UserModel.java
@@ -0,0 +1,25 @@
+package com.imposto.model;
+
+import com.imposto.dto.UserRequestDTO;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "users")
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<RoleModel> roles;
+
+}

c95b21f5a270145dff3b42b3c656d21c1de56202
matheus <matheus.pereira@zup.com.br>
2025-03-06 14:42:57 -0300

    feat: add exception handler

---
+++ src/main/java/com/imposto/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.imposto.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Um erro inesperado aconteceu: " + ex.getMessage());
+    }
+    @ExceptionHandler(NullArgumentionException.class)
+    public ResponseEntity<String> handlerNullArgumentionException(NullArgumentionException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("o argumento foi passada como nulo ou inválido" + ex.getMessage());
+    }
+}

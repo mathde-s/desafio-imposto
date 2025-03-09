a577cab1136b4666d579ca3208b432315f0050c6
matheus <matheus.pereira@zup.com.br>
2025-03-09 20:50:15 -0300

    feat: add implementation of method login

--- src/main/java/com/imposto/service/User/UserService.java
+++ src/main/java/com/imposto/service/User/UserService.java
@@ -3,10 +3,37 @@ package com.imposto.service.User;
 import com.imposto.dto.LoginDTO;
 import com.imposto.dto.UserRequestDTO;
 import com.imposto.dto.UserResponseDTO;
+import com.imposto.exceptions.ExistingResourceException;
+import com.imposto.mapper.UserMapper;
+import com.imposto.model.RoleModel;
+import com.imposto.model.UserModel;
+import com.imposto.repository.RoleRepository;
+import com.imposto.repository.UserRepository;
+import com.imposto.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements IUser{
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
 
     public UserResponseDTO registerUser(UserRequestDTO userRequestDTO){
         if (userRepository.existsByUsername(userRequestDTO.getUsername()))
@@ -26,6 +53,11 @@ public class UserService implements IUser{
     }
 
     public String login(LoginDTO loginDTO) {
-        return "";
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(
+                        loginDTO.getUsername(),
+                        loginDTO.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return jwtTokenProvider.generateToken(authentication);
     }
 }

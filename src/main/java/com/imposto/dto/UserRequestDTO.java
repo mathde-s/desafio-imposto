7f10a90416493ab8869deb48d6811a4aa2265bec
matheus <matheus.pereira@zup.com.br>
2025-03-09 20:59:29 -0300

    refactor: add Set in attributes Role

--- src/main/java/com/imposto/dto/UserRequestDTO.java
+++ src/main/java/com/imposto/dto/UserRequestDTO.java
@@ -1,12 +1,12 @@
 package com.imposto.dto;
 
-import com.imposto.model.RoleModel;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+
+import java.util.Set;
 
 @Data
 public class UserRequestDTO {
     private String username;
     private String password;
-    private RoleModel roles;
+    private Set<Roles> roles;
 }

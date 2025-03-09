06e043e37c996a161096992eb7ec0037d0a7fa58
matheus <matheus.pereira@zup.com.br>
2025-03-09 20:51:26 -0300

    feat: add methods deleteById and existsByName in interface TaxRepository

--- src/main/java/com/imposto/repository/TaxRepository.java
+++ src/main/java/com/imposto/repository/TaxRepository.java
@@ -4,4 +4,6 @@ import com.imposto.model.TaxModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaxRepository extends JpaRepository<TaxModel, Long> {
+    void deleteById(Long id);
+    boolean existsByName(String name);
 }

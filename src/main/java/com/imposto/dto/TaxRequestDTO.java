fb4a8f13e64ca8c420a36cdfe979e95254aa5a83
matheus <matheus.pereira@zup.com.br>
2025-03-13 19:34:05 -0300

    feat: add validation to taxRequestDTO

--- src/main/java/com/imposto/dto/TaxRequestDTO.java
+++ src/main/java/com/imposto/dto/TaxRequestDTO.java
@@ -1,11 +1,19 @@
 package com.imposto.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class TaxRequestDTO {
+    @NotBlank(message = "o nome não pode estar vazio")
     private String name;
+    @NotBlank(message = "a descrição não pode ser vazia")
+    @Size(max = 500)
     private String description;
+
+    @Positive(message = "a aliquota deve ser positiva")
     private double aliquota;
 
 }

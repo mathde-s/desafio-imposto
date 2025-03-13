eec75a43ac481c01c2c12c6270c1b396da38edad
matheus <matheus.pereira@zup.com.br>
2025-03-13 19:35:29 -0300

    feat: add annotation @NoArgsConstructor in roleModel

--- src/main/java/com/imposto/model/RoleModel.java
+++ src/main/java/com/imposto/model/RoleModel.java
@@ -2,10 +2,12 @@ package com.imposto.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "roles")
 @Data
+@NoArgsConstructor
 public class RoleModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,7 +15,5 @@ public class RoleModel {
     private String name;
     public RoleModel(String name) {
         this.name = name;
-    }
-    public RoleModel(){
     }
 }

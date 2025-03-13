7533449043afe374f17986392160e22fcc66df6d
matheus <matheus.pereira@zup.com.br>
2025-03-13 19:38:34 -0300

    feat: adds interface RoleRepository

---
+++ src/main/java/com/imposto/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.imposto.repository;
+
+import com.imposto.model.RoleModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<RoleModel, Long> {
+}

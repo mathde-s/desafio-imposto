10e245830ed77c75873568559fbf16dc4dffe891
matheus <matheus.pereira@zup.com.br>
2025-03-16 16:51:04 -0300

    feat: add annotation AllArgsConstructor in dto UserRequest

--- src/main/java/com/imposto/dto/UserRequestDTO.java
+++ src/main/java/com/imposto/dto/UserRequestDTO.java
@@ -1,10 +1,12 @@
 package com.imposto.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.Set;
 
 @Data
+@AllArgsConstructor
 public class UserRequestDTO {
     private String username;
     private String password;

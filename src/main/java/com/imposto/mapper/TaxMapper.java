382a9392fa00dfe2e2ea2bd5f8cb9ed03cc42ae0
matheus <matheus.pereira@zup.com.br>
2025-03-16 19:39:57 -0300

    refactor: remove static status to method toEntity and toResponse in class TaxMapper

--- src/main/java/com/imposto/mapper/TaxMapper.java
+++ src/main/java/com/imposto/mapper/TaxMapper.java
@@ -3,12 +3,14 @@ package com.imposto.mapper;
 import com.imposto.dto.TaxRequestDTO;
 import com.imposto.dto.TaxResponseDTO;
 import com.imposto.model.TaxModel;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TaxMapper {
-    public static TaxModel toEntity(TaxRequestDTO taxRequestDTO){
+    public TaxModel toEntity(TaxRequestDTO taxRequestDTO){
         return new TaxModel(taxRequestDTO.getName(), taxRequestDTO.getDescription(), taxRequestDTO.getAliquota());
     }
-    public static TaxResponseDTO toResponse(TaxModel taxModel){
+    public TaxResponseDTO toResponse(TaxModel taxModel){
         return new TaxResponseDTO(taxModel.getId(), taxModel.getName(), taxModel.getDescription(), taxModel.getAliquota());
     }
 }

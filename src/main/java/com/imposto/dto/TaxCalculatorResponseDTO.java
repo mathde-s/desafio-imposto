1dd89877013efaae2ac8430dffe0c91ce18596ae
matheus <matheus.pereira@zup.com.br>
2025-03-18 13:59:11 -0300

    feat: add annotation @NoArgsConstructor in dto TaxCalculatorResponse

--- src/main/java/com/imposto/dto/TaxCalculatorResponseDTO.java
+++ src/main/java/com/imposto/dto/TaxCalculatorResponseDTO.java
@@ -1,8 +1,10 @@
 package com.imposto.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class TaxCalculatorResponseDTO {
     private String TaxType;
     private Double baseValue;
